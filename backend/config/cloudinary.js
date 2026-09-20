04e5e389f8578d4eefe4ee549b79751af84af77c
mrharsha96k <moreharshad304@gmail.com>
2026-09-21 02:58:04 +0530

    Secure production environment configuration

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,9 +1,19 @@
+// const cloudinary = require("cloudinary").v2;
+
+// cloudinary.config({
+//     cloud_name: "ekgeesvf",
+//     api_key: "569816769279535",
+//     api_secret: "7X1-fbjbehWs4--_cR6Jkj2YuvU",
+// });
+
+// module.exports = cloudinary;
+
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: "ekgeesvf",
-    api_key: "569816769279535",
-    api_secret: "7X1-fbjbehWs4--_cR6Jkj2YuvU",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 module.exports = cloudinary;
--- backend/config/firebase.js
+++ backend/config/firebase.js
@@ -1,7 +1,24 @@
+// const { initializeApp, cert } = require("firebase-admin/app");
+// const { getFirestore } = require("firebase-admin/firestore");
+
+// const serviceAccount = require("./firebase-service-account.json");
+
+// initializeApp({
+//     credential: cert(serviceAccount),
+// });
+
+// const db = getFirestore();
+
+// module.exports = db;
 const { initializeApp, cert } = require("firebase-admin/app");
 const { getFirestore } = require("firebase-admin/firestore");
 
-const serviceAccount = require("./firebase-service-account.json");
+const serviceAccount = JSON.parse(
+    Buffer.from(
+        process.env.FIREBASE_SERVICE_ACCOUNT_BASE64,
+        "base64"
+    ).toString("utf8")
+);
 
 initializeApp({
     credential: cert(serviceAccount),
